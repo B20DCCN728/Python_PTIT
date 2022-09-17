903c5ad544bc81ebcd92ad601237f0e993f10805
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-09-18 00:52:41 +0700

    Add files via upload

--- Uoc_So_Chung_Nguyen_To.py
+++ Uoc_So_Chung_Nguyen_To.py
@@ -1,4 +1,5 @@
 import math
+#
 def LCM(a, b):
     if a == 0 or b == 0:
         return a + b
@@ -6,21 +7,21 @@ def LCM(a, b):
         if a > b: a -= b
         else: b -= a
     return a
-
+#
 def checkSum(n):
     count = 0
     while n > 0:
         count += n % 10
         n //= 10
     return count
-
+#
 def isPrime(n):
     if n == 1 or n == 0: return False
     if n == 2: return True
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0: return False
     return True
-
+#
 if __name__ == "__main__":
     t = int(input())
     while t > 0:
