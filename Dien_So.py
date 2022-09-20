21a9797f3ac16e7c1be010236c79b72624440b75
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-09-20 09:18:49 +0700

    Add files via upload

---
+++ Dien_So.py
@@ -0,0 +1,13 @@
+from lib2to3.pytree import convert
+
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        n = int(input())
+        solve = set(list(int(x) for x in input().split()))
+        if len(solve) <= 1: print("0")
+        else:
+            convert_list = list(solve)
+            print( - convert_list[0] + convert_list[len(convert_list) - 1] + 1 - len(solve))
+        t -= 1
