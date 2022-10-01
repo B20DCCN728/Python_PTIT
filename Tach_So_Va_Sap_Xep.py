01df7bf433223a96af977b5218f3e86724baf718
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-10-01 20:33:47 +0700

    Add files via upload

---
+++ Tach_So_Va_Sap_Xep.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    t = int(input())
+    data = []
+    while t > 0:
+        solve = input()
+        x = ""
+        for i in range(len(solve)):
+            if solve[i] >= "0" and solve[i] <= "9":
+                x += solve[i]
+            else: continue
+            if i + 1 == len(solve) or solve[i + 1] < "0" or solve[i + 1] > "9":
+                data.append(int(x))
+                x = ""
+        t -= 1
+    data.sort()
+    for i in data: print(i)
