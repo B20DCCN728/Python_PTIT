11377a462471a62a49d46d4a977b73d28737bde9
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-10-02 10:13:35 +0700

    Update PY02054_Bien_Doi_Ve_Ma_Tran_Vuong.py

--- PY02054_Bien_Doi_Ve_Ma_Tran_Vuong.py
+++ PY02054_Bien_Doi_Ve_Ma_Tran_Vuong.py
@@ -27,17 +27,18 @@
             print(res[i][j], end = " ")
         print()'''
 
-n, m = map(int, input().split())
-a = []
-for _ in range(n):
-    a.append([int(i) for i in input().split()])
-tmp = 0 if n > m else 1
-pos = [i for i in range(tmp, abs(n - m) * 2, 2)]
-for i in range(n):
-    if (n > m and i not in pos) or n == m:
-        print(*a[i])
-    elif n < m:
-        for j in range(m):
-            if j not in pos:
-                print(a[i][j], end=' ')
-        print()
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    a = []
+    for _ in range(n):
+        a.append([int(i) for i in input().split()])
+    tmp = 0 if n > m else 1
+    pos = [i for i in range(tmp, abs(n - m) * 2, 2)]
+    for i in range(n):
+        if (n > m and i not in pos) or n == m:
+            print(*a[i])
+        elif n < m:
+            for j in range(m):
+                if j not in pos:
+                    print(a[i][j], end=' ')
+            print()
