83a2873a5418ef538aa10be8f931893420ea62a8
Nguyễn Hoàng Việt <108795111+Campus-is-me@users.noreply.github.com>
2022-12-14 22:21:50 +0700

    Create 0_Sang_Nguyen_To.py

---
+++ 0_Sang_Nguyen_To.py
@@ -0,0 +1,8 @@
+#Sanng Nguyen To Pro Max
+era = [0] * 1000001
+def era_pro_max():
+    era[0] = era[1] = 1
+    for i in range(2, 1000):
+        if era[i] == 0:
+            for j in range(i * i, 1000001, i):
+                era[j] = 1
